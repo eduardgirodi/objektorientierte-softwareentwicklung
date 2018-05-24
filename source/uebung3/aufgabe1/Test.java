b71b13dc7cf7f4eaf270a460066d25d76df143a9
Eduard Girodi <eduard@girodi.de>
2018-05-24 22:41:16 +0200

    Modify Test.java from exercise 3 task 1

--- source/uebung3/aufgabe1/Test.java
+++ source/uebung3/aufgabe1/Test.java
@@ -8,12 +8,10 @@ public class Test {
         ((C1) b).f1();
         System.out.println(a.s);
         b.f1();
-          b.f2();
+        b.f2();
         b.f3();
 //        C1 c = new C1(); c.f3();
 //        C1 c = new C1(); ((C2) c).f1();
-//        a.f2();
-//        a.f1();
     }
 }
 
@@ -24,6 +22,6 @@ public class Test {
 4.  C2::f1
 5.  C1::f2
 6.  C2::f3
-7.
-8.
- */
+7.  Compile-/Runtime-Error
+8.  Compile-/Runtime-Error
+*/
