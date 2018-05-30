459d968bce153037f3fb12586783d990f1f65ddf
Eduard Girodi <eduard@girodi.de>
2018-05-30 14:19:26 +0200

    Refactor code in source/uebung4/aufgabe3/Main.java

--- source/uebung4/aufgabe3/Main.java
+++ source/uebung4/aufgabe3/Main.java
@@ -4,10 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         Cauliflower essen1 = new Cauliflower();
-        Student.eat(essen1);                        //a) "I don't like Blumenkoh an Gorgonzolasoße"
+        Student.eat(essen1);                        //a) "I don't like Blumenkohl an Gorgonzolasoße"
 
         Spaghetti essen2 = new Spaghetti();
-        Student.eat(essen2);                        //b) "I like Spaghetti Bolognerser Art"
+        Student.eat(essen2);                        //b) "I like Spaghetti Bologneser Art"
 
         Food essen = new Cauliflower();
         Student.eat(essen);                         //c) "I like Blumenkohl an Gorgonzolasoße
