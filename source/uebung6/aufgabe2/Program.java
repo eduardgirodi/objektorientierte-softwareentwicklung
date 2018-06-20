484cde1830844fc1433ab04ab0abf74e79a51061
Eduard Girodi <eduard@girodi.de>
2018-06-20 14:27:18 +0200

    Refeactor code for solutions of exercise 6

--- source/uebung6/aufgabe2/Program.java
+++ source/uebung6/aufgabe2/Program.java
@@ -1,10 +1,12 @@
 package uebung6.aufgabe2;
 
+import java.io.File;
+
 public class Program
 {
 	public static void main(String[] args)
 	{
-		Thread t = new Thread(new DateiBeobachter("\\home\\eduard\\Documents\\Test.txt"));
+		Thread t = new Thread(new DateiBeobachter(File.separator +"home" +File.separator +"eduard" +File.separator +"Documents" +File.separator +"Test.txt"));
 		t.start();
 	}
 }
